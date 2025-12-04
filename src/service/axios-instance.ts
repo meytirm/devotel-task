9973435a77e6f1b755941cd4dcf13c482f7add54
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-05 00:15:11 +0330

    chore(service): create Axios instance with basic CRUD methods

---
+++ src/service/axios-instance.ts
@@ -0,0 +1,39 @@
+import axios, {type AxiosInstance, type AxiosRequestConfig} from "axios";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: "https://dummyjson.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }
+})
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  (error) => error
+)
+
+export function get<ResponseType>(url: string, config: AxiosRequestConfig = {}) {
+  return axiosInstance.get<ResponseType>(url, config)
+}
+
+export function post<ResponseType, RequestType = never>(
+  url: string,
+  data: RequestType,
+  config?: AxiosRequestConfig
+) {
+  return axiosInstance.post<ResponseType>(url, data, config)
+}
+
+export function put<ResponseType, RequestType = never>(
+  url: string,
+  data: RequestType,
+  config?: AxiosRequestConfig
+) {
+  return axiosInstance.put<ResponseType>(url, data, config)
+}
+
+export function del<ResponseType>(url: string, config?: AxiosRequestConfig) {
+  return axiosInstance.delete<ResponseType>(url, config)
+}
