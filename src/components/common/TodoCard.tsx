3bd7beddf65996a6d3a9d421e5ff2a0687d39f6e
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-06 01:39:56 +0330

    feat(todos): implement drag-and-drop functionality in TodoCard component

--- src/components/common/TodoCard.tsx
+++ src/components/common/TodoCard.tsx
@@ -1,16 +1,32 @@
 import {CheckIcon, LoaderCircleIcon, SquarePenIcon, Trash2Icon} from "lucide-react";
+import {useSortable} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const TodoCard = ({name, completed, onRemove, onEdit, onCompleteToggle, id, selectedId, loading}: Props) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
+  };
   return (
-    <div className="flex justify-between items-center min-h-[75px] w-full rounded-md bg-brand/10 px-4">
+    <div className="flex justify-between items-center min-h-[75px] w-full rounded-md bg-brand/10 px-4 cursor-move active:cursor-grabbing" ref={setNodeRef}
+         style={style}
+         {...attributes}
+         {...listeners}>
       <div className="flex items-center gap-2">
         {id === selectedId && loading ? <LoaderCircleIcon className="animate-spin text-brand" /> : <CheckIcon size={20} className={`${completed ? 'text-green-400' : 'text-gray-300'} cursor-pointer`}
-                    onClick={onCompleteToggle}/>}
+                    onMouseDown={onCompleteToggle}/>}
         <div className={completed ? 'line-through text-gray-500' : 'font-bold'}>{name}</div>
       </div>
       <div className="flex items-center gap-2">
-        <span onClick={onEdit} className="cursor-pointer"><SquarePenIcon size={16} className="text-blue-400" /></span>
-        <span onClick={onRemove} className="cursor-pointer"><Trash2Icon size={16} className="text-red-400" /></span>
+        <span onMouseDown={onEdit} className="cursor-pointer"><SquarePenIcon size={16} className="text-blue-400" /></span>
+        <span onMouseDown={onRemove} className="cursor-pointer"><Trash2Icon size={16} className="text-red-400" /></span>
       </div>
     </div>
   )
