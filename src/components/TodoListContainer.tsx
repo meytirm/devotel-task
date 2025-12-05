2d678213802c3d78518bedbec9ea4ded9c5b0579
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-05 17:34:05 +0330

    feat(ui): integrate Redux state management into TodoListContainer
    - Connected `TodoListContainer` to Redux store using `useSelector` and `useDispatch`.
    - Added `useEffect` to dispatch `setTodos` action with fetched data.

--- src/components/TodoListContainer.tsx
+++ src/components/TodoListContainer.tsx
@@ -2,12 +2,20 @@ import {useFindAllTodosQuery} from "../hooks/todo/useFindAllTodosQuery.ts";
 import TodoCard from "./common/TodoCard.tsx";
 import NetworkError from "./common/NetworkError.tsx";
 import NetworkLoading from "./common/NetworkLoading.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import type {RootState} from "../store";
+import {setTodos} from "../store/todo/todoSlice.ts";
+import {useEffect} from "react";
 
 const TodoListContainer = () => {
+  const todos = useSelector((state: RootState) => state.todos)
+  const dispatch = useDispatch()
   const {data, isError, isLoading, refetch} = useFindAllTodosQuery()
 
 
-  const todos = data ? data.todos : []
+  useEffect(() => {
+      dispatch(setTodos(data ? data.todos : []))
+  }, [data, dispatch])
 
   if (isError) {
     return <NetworkError onReload={() => refetch()} />
