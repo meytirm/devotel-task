a751736e1bc2e1d4fb3ed3e2c4c701570262b882
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-05 13:48:24 +0330

    feat(ui): add TodoApp component with layout and basic functionality

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,23 @@
+import Input from "./ui/Input.tsx";
+import Button from "./ui/Button.tsx";
+import TodoCard from "./common/TodoCard.tsx";
+
+const TodoApp = () => {
+  return <div className="flex justify-center items-center w-full h-[100vh]">
+    <div className="w-2/5 rounded-md p-4 flex flex-col justify-center items-center gap-4">
+      <h1 className="font-bold">Todo List</h1>
+      <div className="flex justify-center items-center gap-4 mt-4">
+        <Input className="border-brand" placeholder="Add a new task" />
+        <Button className="bg-brand  hover:bg-brand/80 text-white">Add</Button>
+      </div>
+      <div className="border border-gray-300 rounded-md p-4 w-full flex flex-col gap-4">
+        <div className="">
+          <Input className="" placeholder="Search..." />
+        </div>
+        <TodoCard />
+      </div>
+    </div>
+  </div>
+}
+
+export default TodoApp
