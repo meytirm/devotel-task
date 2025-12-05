002f6d1003f9ac108af88bd901326ec808c12c98
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-05 14:41:01 +0330

    feat(ui): add icons to TodoCard for edit and remove actions

--- src/components/common/TodoCard.tsx
+++ src/components/common/TodoCard.tsx
@@ -1,4 +1,5 @@
 import Input from "../ui/Input.tsx";
+import {SquarePenIcon, Trash2Icon} from "lucide-react";
 
 const TodoCard = ({name, completed}: Props) => {
   return (
@@ -7,9 +8,9 @@ const TodoCard = ({name, completed}: Props) => {
         <Input type="checkbox" checked={completed} />
         <div>{name}</div>
       </div>
-      <div className="flex gap-2">
-        <span>edit</span>
-        <span>remove</span>
+      <div className="flex items-center gap-2">
+        <span><SquarePenIcon size={16} className="text-blue-400" /></span>
+        <span><Trash2Icon size={16} className="text-red-400" /></span>
       </div>
     </div>
   )
