cf2e06130d7ec7e79e703d155fea0952cffb806d
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-05 11:39:51 +0330

    feat(ui): improve Input component with `tailwind-merge` for better class merging

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,8 +1,15 @@
 import type {InputHTMLAttributes} from "react";
+import {twMerge} from "tailwind-merge";
+
 
 const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  const {className, ...rest} = props;
   return (
-    <input type="text" {...props} />
+    <input type="text" {...rest} className={twMerge(
+      'border-2 border-gray-300 rounded-md p-2 hover:border-gray-500 focus:border-gray-300',
+      className
+    )}
+    />
   )
 }
 
