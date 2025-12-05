975096dd4f61dee3a9bdfee91f2426976e95d034
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-12-06 00:47:49 +0330

    feat(ui): integrate search and filter functionality in TodoApp with server data

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,8 +1,30 @@
 import Input from "./ui/Input.tsx";
 import TodoListContainer from "./TodoListContainer.tsx";
 import CreateTodoForm from "./common/CreateTodoForm.tsx";
+import {useFindAllTodosQuery} from "../hooks/todo/useFindAllTodosQuery.ts";
+import {useEffect, useState} from "react";
+import type {TodoType} from "../types/todos.ts";
+import {setTodos} from "../store/todo/todoSlice.ts";
+import {useDispatch, useSelector} from "react-redux";
+import type {AppDispatch, RootState} from "../store";
 
 const TodoApp = () => {
+  const [search, setSearch] = useState<string>("")
+  const {data, isError, isLoading, refetch} = useFindAllTodosQuery()
+  const todos = useSelector((state: RootState) => state.todos)
+  const dispatch = useDispatch<AppDispatch>()
+
+
+
+  useEffect(() => {
+    dispatch(setTodos(data ? data.todos : []))
+  }, [data, dispatch])
+
+  const normalizedSearch = search.trim().toLowerCase()
+  const filteredTodos = todos.filter((todo: TodoType) => {
+    const text = (todo.todo).toString().toLowerCase()
+    return !(normalizedSearch && !text.includes(normalizedSearch));
+  })
 
   return <div className="flex justify-center items-center w-full">
     <div className="w-2/5 rounded-md p-4 flex flex-col justify-center items-center gap-4">
@@ -10,9 +32,14 @@ const TodoApp = () => {
       <CreateTodoForm/>
       <div className="border border-gray-300 rounded-md p-4 w-full flex flex-col gap-4">
         <div className="">
-          <Input className="" placeholder="Search..."/>
+          <Input className="" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)}/>
         </div>
-        <TodoListContainer/>
+        <TodoListContainer
+          todos={filteredTodos}
+          isError={isError}
+          isLoading={isLoading}
+          refetch={refetch}
+        />
       </div>
     </div>
   </div>
